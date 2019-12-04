2f1c02ef330f26e247b9c00b0e1ad888b055848c
Longze SU <lsu018@ucr.edu>
2019-12-03 22:03:55 -0800

    newnew

--- preprocessing/spark_rdd/src/main/java/pre_process/execute.java
+++ preprocessing/spark_rdd/src/main/java/pre_process/execute.java
@@ -25,6 +25,6 @@ public class execute {
         w.count(input, pType, Type.location, args[7]);
         w.count(input, pType, Type.community, args[8]);
         w.count(input, pType, Type.month, args[5]);
-        w.count(input, pType, Type.day, args[6]);
+        w.count(input, pType, Type.day, args[6]); 
     }
 }
